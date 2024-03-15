6f71d873726fb8e5a7d2f139353e306ebaed9c8d
devbn3li <fudwhc@gmail.com>
2024-03-15 14:42:40 +0200

    fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a function that determines if a box containing a list
+of lists can be opened using keys
+"""
+
+
 def canUnlockAll(boxes):
-    if len(boxes) == 0:
-        return False
+    """return trure if all boxes unlocked"""
+    unlocked_boxes = [0]
+    keys = list(boxes[0])
 
-    # Initialize a set to keep track of visited boxes
-    visited = set()
-    # Initialize a queue for BFS
-    queue = [0]
+    while keys:
+        key = keys.pop()
+        if key < len(boxes) and key not in unlocked_boxes:
+            unlocked_boxes.append(key)
+            keys.extend(boxes[key])
 
-    while queue:
-        # Pop the first box from the queue
-        current_box = queue.pop(0)
-        # Mark the current box as visited
-        visited.add(current_box)
-
-        # Check all the keys in the current box
-        for key in boxes[current_box]:
-            # If the key opens a new box and that box has not been visited yet, add it to the queue
-            if key < len(boxes) and key not in visited:
-                queue.append(key)
-
-    # If all boxes have been visited, return True, otherwise, return False
-    return len(visited) == len(boxes)
+    return len(unlocked_boxes) == len(boxes)
